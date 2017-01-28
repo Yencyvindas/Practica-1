cfee52f5ee8f1d9d9d9aaba3ffb01d4f4719b83c
Yency Vindas <yevinagui@gmail.com>
2017-01-28 13:51:41 -0600

    crear caracteristica de un animal

---
+++ src/practica/pkg1/Animal.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica.pkg1;
+
+/**
+ *
+ * @author Yency Vindas Aguilar
+ */
+public class Animal {
+   private String nombre; 
+   private int edad;
+   private String  raza;
+   private String color;
+   
+   public Animal(String nombreN){
+       nombre = nombreN;
+   
+   }
+   public String getNombre(){
+       return nombre;
+   }
+   
+      public int getEdad(){
+          return edad;
+      }     
+      public void setEdad(int EdadN){
+          edad = EdadN;
+      }
+}
